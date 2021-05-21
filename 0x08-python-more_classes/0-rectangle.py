b946202cc6cdb3f011db115db2a4fbdb4aef7ab6
Hugo Cortes <hugocortesmu@gmail.com>
2021-05-20 22:30:33 -0500

    more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Creating an empty class that defines a rectangle
+'''
+
+
+class Rectangle:
+    '''Empty class called Rectangle'''
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+# 0x08. Python - More Classes and Objects :snake:
+
+Foundations - Higher-level programming ― Python
+ By Guillaume, CTO at Holberton School
+2021
