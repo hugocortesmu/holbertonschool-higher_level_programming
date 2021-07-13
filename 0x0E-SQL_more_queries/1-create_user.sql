824fc296e6dcd1ca7f95cb07516df3408126c2d5
Hugo Cortes <hugocortesmu@gmail.com>
2021-07-13 16:21:39 -0500

    more queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- SQL Project
+-- Write a script that creates the MySQL server user user_0d_1
+CREATE DATABASE IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost'
