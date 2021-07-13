5c29af9994651c08bfa5b837f19cf7f80f1df053
Hugo Cortes <hugocortesmu@gmail.com>
2021-07-12 23:35:00 -0500

    fixed temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,3 @@
--- and finds first 3 cities with average depending on month
+-- finds average temparature
 SELECT city, AVG(value) AS avg_temp FROM `temperatures`
-WHERE month BETWEEN 7 AND 8
-GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+GROUP BY city ORDER BY avg_temp DES
