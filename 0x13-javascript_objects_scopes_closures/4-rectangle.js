07a4a93ce5a721f66ed60fbee33b23fce3f99a99
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-03 21:00:09 -0500

    javascript_objects

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+// Write a  class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myRow = '';
+      for (let j = 0; j < this.width; j++) {
+        myRow += 'X';
+      }
+      console.log(myRow);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
