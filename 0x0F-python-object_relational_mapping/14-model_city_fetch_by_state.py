ea5e941f9c7007898495b4b44cae5bcbcc6217a5
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-11 23:04:02 -0500

    filters and state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,25 +2,21 @@
 """
 script that prints all City objects from the database
 """
-from sys import argv
-from model_state import Base, State
-from model_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2],
-                                  argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State, City).filter(State.id == City.state_id).all()
-    for result in query:
-        print("{}: ({}) {}".
-              format(result.State.name, result.City.id, result.City.name))
-    session.close()
+
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
