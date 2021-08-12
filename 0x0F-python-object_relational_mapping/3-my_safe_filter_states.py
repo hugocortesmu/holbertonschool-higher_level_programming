c13803cd4d45c6c52bda6cf5da0a111cc733fc3c
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-11 22:51:19 -0500

    filters and states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,25 +3,18 @@
 write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
 """
 
-
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    """Connect to a MySQL server."""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE Name = %s ORDER BY id ASC",
+                [sys.argv[4]])
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306)
-
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s\
-    ORDER BY id ASC".format(argv[4],))
-    states = cursor.fetchall()
-    for state in states:
-        if state[1] == argv[4]:
-            print(state)
-    cursor.close()
-    db.close()
