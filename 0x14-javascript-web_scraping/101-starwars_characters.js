2011bd7ca2f96bb7ee176c46d12f4caf41ae727c
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-15 23:52:18 -0500

    js web

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// write script that computes the number of tasks completed by user id
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const role = JSON.parse(body).characters;
+    print(role, 0);
+  }
+});
+
+function print (array, i) {
+  if (i >= array.length) {
+    return;
+  }
+  request.get(array[i], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+    }
+    return print(array, i + 1);
+  });
+}
