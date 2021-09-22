705491207b7bbd8fd9b9248fd8d5233f5bad6178
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-22 18:10:18 -0500

    remove and add

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -5,4 +5,7 @@ $('document').ready(function () {
   $('DIV#remove_item').click(function () {
     $('UL.my_list li:last').remove();
   });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
+  });
 });
